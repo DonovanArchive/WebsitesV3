bb4a9e01a35b1939ed9bf5dbed54d5be46d94882
Donovan Daniels <hewwo@yiff.rocks>
2022-09-15 14:51:48 -0500

    base page (oceanic.ws)

--- src/sites/oceanic.ws/index.ts
+++ src/sites/oceanic.ws/index.ts
@@ -1,6 +1,6 @@
 import docsRoute from "./routes/docs";
 import Website from "@lib/Website";
-import { Router } from "express";
+import express, { Router } from "express";
 
 export default class FurryCool extends Website {
 	constructor() {
@@ -12,6 +12,10 @@ export default class FurryCool extends Website {
 			.init();
 
 		this
-			.addSubdomain("docs", Router().use(docsRoute));
+			.addSubdomain("docs", Router().use(docsRoute))
+			.addSubdomain("i", express.static("/app/public/images"))
+			.addHandler(
+				express.Router()
+					.get("/", async(req, res) => res.render("index", { year: new Date().getFullYear(), layout: false }))
 	}
 }

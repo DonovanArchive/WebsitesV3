ae1a1cac0dbb0b84428bc7ba1122ceb860ecdda9
Donovan Daniels <hewwo@yiff.rocks>
2022-11-28 19:03:46 -0600

    do the dew

--- src/sites/yiff.rest/routes/thumbs.ts
+++ src/sites/yiff.rest/routes/thumbs.ts
@@ -50,9 +50,9 @@ app
 			if (!post) return res.status(404).json({ success: false, error: "Invalid Post ID" });
 			md5 = post.file.md5;
 		} else md5 = req.params.id;
+		if (!/[a-f\d]{32}/i.test(md5)) return res.status(404).json({ success: false, error: "Invalid MD5" });
 		const gifExists = await exists(filePath(md5, "gif"));
 		const pngExists = await exists(filePath(md5, "png"));
-		if (!gifExists && !pngExists) return res.status(404).json({ success: false, error: "Not Found" });
 		return res.status(200).json({
 			success: true,
 			gifURL:  gifExists ? url(md5, "gif") : null,
@@ -66,20 +66,23 @@ app
 			if (!post) return res.status(404).json({ success: false, error: "Invalid Post ID" });
 			md5 = post.file.md5;
 		} else md5 = req.params.id;
+		if (!/[a-f\d]{32}/i.test(md5)) return res.status(404).json({ success: false, error: "Invalid MD5" });
 		const type = req.params.type.toLowerCase() as "gif" | "png";
 		if (!["gif", "png"].includes(type)) return res.status(404).json({ success: false, error: "Invalid Type" });
 		const existing = await exists(filePath(md5, type));
-		if (existing) return res.status(200).json({ success: true, url: url(md5, type) });
+		if (existing) return res.status(200).json({ success: true, status: "done", url: url(md5, type) });
 		const inQueue = E621Thumbnails.has(md5, type);
 		if (inQueue) {
 			const check = E621Thumbnails.check(md5, type)!;
+			if (check.status === "error") return res.status(500).json({ success: false, status: "error" });
 			return res.status(202).json({ success: true, ...check });
 		}
 		const check = E621Thumbnails.add(md5, type);
-		return res.status(202).json({ success: true, status: "processing", checkURL: check.url, time: check.time, startedAt: check.startedAt });
+		return res.status(202).json({ success: true, status: "processing", checkURL: check.url, checkAt: Date.now() + check.time, time: check.time, startedAt: check.startedAt });
 	})
 	.get("/check/:md5/:type", async(req,res) => {
 		const md5 = req.params.md5;
+		if (!/[a-f\d]{32}/i.test(md5)) return res.status(404).json({ success: false, error: "Invalid MD5" });
 		const type = req.params.type.toLowerCase() as "gif" | "png";
 		if (!["gif", "png"].includes(type)) return res.status(404).json({ success: false, error: "Invalid Type" });
 		const existing = await exists(filePath(md5, type));

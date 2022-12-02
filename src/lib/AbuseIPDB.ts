5df9825e74405d68050b3c5cc64245a02ebc5fc1
Donovan Daniels <hewwo@yiff.rocks>
2022-12-02 04:10:18 -0600

    you've activated my trap card

--- src/lib/AbuseIPDB.ts
+++ src/lib/AbuseIPDB.ts
@@ -22,6 +22,12 @@ export default class AbuseIPDB {
 	static async check(ip: string) {
 		const cache = await db.r.get(`abuseipdb:${ip}`);
 		if (cache !== null) return cache === "true";
+		const block = (await this.get(ip)) > 50;
+		await db.r.set(`abuseipdb:${ip}`, String(block), "EX", 60 * 60 * 24);
+		return block;
+	}
+
+	static async get(ip: string) {
 		const response = await fetch(`https://api.abuseipdb.com/api/v2/check?ipAddress=${ip}`, {
 			headers: {
 				Accept: "application/json",
@@ -29,8 +35,18 @@ export default class AbuseIPDB {
 			}
 		});
 
-		const block = (await response.json() as { data: IPCheckResult; }).data.abuseConfidenceScore > 50;
-		await db.r.set(`abuseipdb:${ip}`, String(block), "EX", 60 * 60 * 24);
-		return block;
+		return (await response.json() as { data: IPCheckResult; }).data.abuseConfidenceScore;
+	}
+
+	static async report(ip: string, categories: Array<number>, comment: string) {
+		await fetch("https://api.abuseipdb.com/api/v2/report", {
+			method:  "POST",
+			headers: {
+				"Accept":       "application/json",
+				"Key":          services.abuseipdb,
+				"Content-Type": "application/x-www-form-urlencoded"
+			},
+			body: `ip=${ip}&categories=${categories.join(",")}&comment=${encodeURIComponent(comment)}`
+		});
 	}
 }

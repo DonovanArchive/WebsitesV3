1d89447c184fa53290051770f167f26fd87b680d
Donovan Daniels <hewwo@yiff.rocks>
2022-12-14 18:10:41 -0600

    handle timeouts & other random errors when fetching e621 status

--- src/sites/e621.ws/index.ts
+++ src/sites/e621.ws/index.ts
@@ -1,19 +1,35 @@
+import Logger from "../../util/Logger";
 import Website from "@lib/Website";
 import express from "express";
 import { fetch } from "undici";
+import { RequestAbortedError } from "undici/types/errors";
 import { access, readFile, writeFile } from "fs/promises";
 import { STATUS_CODES } from "http";
 
 async function check() {
-	const run = await fetch("https://e621.net/posts.json?limit=0", {
-		headers: {
-			"User-Agent": "E621Status/1.0.0 (https://status.e621.ws; \"donovan_dmc\")"
-		},
-		method: "HEAD"
-	});
+	let status: number;
+	try {
+		const controller = new AbortController();
+		const timeout = setTimeout(() => controller.abort(), 3000);
+		status = (await fetch("https://e621.net/posts.json?limit=0", {
+			headers: {
+				"User-Agent": "E621Status/1.0.0 (https://status.e621.ws; \"donovan_dmc\")"
+			},
+			method: "HEAD",
+			signal: controller.signal
+		})).status;
+		clearTimeout(timeout);
+	} catch (err) {
+		if (err instanceof RequestAbortedError) {
+			status = 408;
+		} else {
+			status = 0;
+			Logger.getLogger("e621.ws").error(err);
+		}
+	}
 	const old = await get(true);
 
-	return run.status === old.status ? old : write(run.status);
+	return status === old.status ? old : write(status);
 }
 
 async function get(noLoop = false): Promise<{ status: number; since: string; }> {
@@ -53,6 +69,7 @@ async function write(status: number): Promise<{ status: number; since: string; }
 }
 
 const notes: Record<number, string> = {
+	0:   "Some internal issue happened while contacting e621.",
 	503: "E621 is likely experiencing some kind of attack right now, so api endpoints may be returning challenges."
 };
 export default class E621WS extends Website {
@@ -75,7 +92,7 @@ export default class E621WS extends Website {
 						return res.status(200).render("status", {
 							time:        since,
 							state:       status >= 200 && status <= 299 ? "up" : status === 503 ? "partially down" : "down",
-							status:      `${status} ${STATUS_CODES[status] || ""}`,
+							status:      `${status} ${STATUS_CODES[status] || status === 0 ? "Internal Error" : ""}`.trim(),
 							statusClass: status >= 200 && status <= 299 ? "success" : status === 503 ? "partially down" : "error",
 							note:        notes[status] === undefined ? "" : `<h3><center>${notes[status]}</center></h3>`
 						});
@@ -87,14 +104,14 @@ export default class E621WS extends Website {
 							current: {
 								state:         current.status >= 200 && current.status <= 299 ? "up" : "down",
 								status:        current.status,
-								statusMessage: STATUS_CODES[current.status] || "",
+								statusMessage: STATUS_CODES[current.status] || current.status === 0 ? "Internal Error" : "",
 								since:         current.since,
 								note:          notes[current.status] ?? null
 							},
 							history: history.map(({ status, since }) => ({
 								state:         status >= 200 && status <= 299 ? "up" : "down",
 								status,
-								statusMessage: STATUS_CODES[status] || "",
+								statusMessage: STATUS_CODES[status] || status === 0 ? "Internal Error" : "",
 								since
 							}))
 						});

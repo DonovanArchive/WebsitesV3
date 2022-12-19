30a23876d941577e62435406b370de9edb54a849
Donovan Daniels <hewwo@yiff.rocks>
2022-12-19 02:16:35 -0600

    more logging

--- scripts/update.ts
+++ scripts/update.ts
@@ -12,6 +12,7 @@ const oldIDs: Array<string> = [], newIPs: Record<string, string> = {};
 for (const name of scaled) {
 	oldIDs.push(execSync(`docker ps -f name=${name} -q | head -n1`).toString().trim());
 }
+console.log("Scaling Up");
 execSync(`docker compose up -d --no-deps ${scale2} --no-recreate ${list}`, { stdio: "inherit" });
 
 for (const name of scaled) {
@@ -20,15 +21,20 @@ for (const name of scaled) {
 	newIPs[name] = newIP;
 }
 
+console.log("Writing New Websites Hosts");
 run(Object.entries(newIPs).map(([name, ip]) => [ip, `${name.replace(/\./g, "-")}.websites.local`]));
 execSync("sudo nginx -s reload", { stdio: "inherit" });
+console.log("Nginx Successfully Reloaded");
 
 for (const id of oldIDs) {
 	execSync(`docker stop ${id}`, { stdio: "inherit" });
 	execSync(`docker rm ${id}`, { stdio: "inherit" });
 }
+console.log("Old Containers Removed");
 
+console.log("Scaling Down");
 execSync(`docker compose up -d --no-deps ${scale1} --no-recreate ${list}`, { stdio: "inherit" });
 execSync("sudo nodenv exec docker-hosts --config /root/.docker-hosts.json", { stdio: "inherit" });
+console.log("Updating Generic Hosts");
 
 console.log("load shifted");

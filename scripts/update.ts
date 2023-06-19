ea522f90f272222df55a1d602a8586959e44af04
Donovan Daniels <hewwo@yiff.rocks>
2023-06-19 16:26:08 -0500

    docker hostnames

--- scripts/update.ts
+++ scripts/update.ts
@@ -1,29 +1,17 @@
-import { run, test } from "./update-hosts";
 import { scaled } from "./shared";
 import { execSync } from "node:child_process";
-
-test();
 
 const scale2 = scaled.map(s => `--scale ${s}=2`).join(" ");
 const scale1 = scaled.map(s => `--scale ${s}=1`).join(" ");
 const list = scaled.join(" ");
 
-const oldIDs: Array<string> = [], newIPs: Record<string, string> = {};
+const oldIDs: Array<string> = [];
 for (const name of scaled) {
 	oldIDs.push(execSync(`docker ps -f name=${name} -q | sed -n '1p'`).toString().trim());
 }
 console.log("Scaling Up");
 execSync(`docker compose up -d --no-deps ${scale2} --no-recreate ${list}`, { stdio: "inherit" });
 
-for (const name of scaled) {
-	const newID = execSync(`docker ps -f name=${name} -q | sed -n '2p'`).toString().trim();
-	const newIP = execSync(`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${newID}`).toString().trim();
-	newIPs[name] = newIP;
-}
-
-console.log("Writing New Websites Hosts");
-
-run(Object.entries(newIPs).map(([name, ip]) => [ip, `${name.replace(/\./g, "-")}.websites.local`]));
 execSync("sudo nginx -s reload", { stdio: "inherit" });
 console.log("Nginx Successfully Reloaded");
 
